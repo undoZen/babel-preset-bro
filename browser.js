56150f30678558fdf7b657057b660be52ce6545e
Anson Yu Wang <opensource@undozen.com>
2015-12-24 04:46:10 +0800

    fix browser.js

--- browser.js
+++ browser.js
@@ -13,7 +13,7 @@ plugins.unshift(
     require("babel-plugin-transform-es2015-for-of"),
     require("babel-plugin-check-es2015-constants"),
     require("babel-plugin-transform-es2015-block-scoping"),
-    require("babel-plugin-transform-es2015-typeof-symbol"),
+    require("babel-plugin-transform-es2015-typeof-symbol")
 );
 
 module.exports = {
